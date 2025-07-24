b171bedd8923d0adcae727850e8e552e48075d14
lomega <atibon3@gmail.com>
2025-07-25 00:38:52 +0900

    init

---
+++ search_arxiv.py
@@ -0,0 +1,25 @@
+import requests
+import feedparser
+
+def search_arxiv_by_title(title):
+    base_url = "https://export.arxiv.org/api/query"
+    query = f'ti:{title} AND au:ryu AND au:chang AND au:you AND au:wen'
+    params = {
+        "search_query": query,
+        "start": 0,
+        "max_results": 10
+    }
+    response = requests.get(base_url, params=params)
+    print(response.url)
+    feed = feedparser.parse(response.text)
+    print(len(feed.entries))
+    for entry in feed.entries:
+        print("Title:", entry.title)
+        print("Authors:", ", ".join(author.name for author in entry.authors))
+        print("arXiv URL:", entry.id)
+        print("PDF URL:", entry.id.replace('/abs/', '/pdf/') + ".pdf")
+        print("---")
+
+# search_arxiv_by_title("Tensor network simulations for non-orientable surfaces")
+search_arxiv_by_title("Entanglement spectrum and entropy in topological non-Hermitian systems")
+
